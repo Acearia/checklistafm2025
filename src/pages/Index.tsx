f93a1d2530f73bf108801ffcd0fbad4726c0503e
acearia <ti@afm.com.br>
2025-12-10 06:42:10 -0300

    browser erro

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ClipboardCheck, Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
